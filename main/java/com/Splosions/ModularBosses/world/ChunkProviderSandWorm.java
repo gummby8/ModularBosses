3e9430c7e4a4d25067e0f50c79fccd9e16603424
Engineer <dsingleton@dci.local>
2016-06-16 13:53:02 -0700

    Dimensions

--- main/java/com/Splosions/ModularBosses/world/ChunkProviderSandWorm.java
+++ main/java/com/Splosions/ModularBosses/world/ChunkProviderSandWorm.java
@@ -1,5 +1,98 @@
 package com.Splosions.ModularBosses.world;
 
-public class ChunkProviderSandWorm {
+import java.util.List;
+
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.IProgressUpdate;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.IChunkProvider;
+
+public class ChunkProviderSandWorm implements IChunkProvider {
+
+	@Override
+	public boolean chunkExists(int x, int z) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Chunk provideChunk(int x, int z) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Chunk provideChunk(BlockPos blockPosIn) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void populate(IChunkProvider p_73153_1_, int p_73153_2_, int p_73153_3_) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean func_177460_a(IChunkProvider p_177460_1_, Chunk p_177460_2_, int p_177460_3_, int p_177460_4_) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean saveChunks(boolean p_73151_1_, IProgressUpdate p_73151_2_) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean unloadQueuedChunks() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canSave() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String makeString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List func_177458_a(EnumCreatureType p_177458_1_, BlockPos p_177458_2_) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BlockPos getStrongholdGen(World worldIn, String p_180513_2_, BlockPos p_180513_3_) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getLoadedChunkCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void recreateStructures(Chunk p_180514_1_, int p_180514_2_, int p_180514_3_) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveExtraData() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
