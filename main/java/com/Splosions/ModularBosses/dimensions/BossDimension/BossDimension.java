3d47710d81341808605c623993da3f173f3fb76a
gummby8 <gummby8@gmail.com>
2016-07-17 16:56:41 -0700

    Dimensions

--- main/java/com/Splosions/ModularBosses/dimensions/BossDimension/BossDimension.java
+++ main/java/com/Splosions/ModularBosses/dimensions/BossDimension/BossDimension.java
@@ -3,24 +3,22 @@ package com.Splosions.ModularBosses.dimensions.BossDimension;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.biome.BiomeGenBase;
 
-public class BossDimension extends BiomeGenBase
-{
-	public BossDimension(int id)
-	{
+public class BossDimension extends BiomeGenBase {
+	public BossDimension(int id) {
 		super(id, true);
 		setBiomeName("BossBiome");
 		this.setHeight(height_LowPlains);
 		this.setTemperatureRainfall(0.5F, 0.3F);
 		this.enableRain = false;
 		this.enableSnow = false;
-        this.theBiomeDecorator.generateLakes = false;
-		
+		this.theBiomeDecorator.generateLakes = false;
+
 		this.spawnableMonsterList.clear();
-	    this.spawnableCreatureList.clear();
-	    this.spawnableWaterCreatureList.clear();
-	    this.spawnableCaveCreatureList.clear();
-	    
-        this.topBlock = Blocks.air.getDefaultState();
-        this.fillerBlock = Blocks.air.getDefaultState();
+		this.spawnableCreatureList.clear();
+		this.spawnableWaterCreatureList.clear();
+		this.spawnableCaveCreatureList.clear();
+
+		this.topBlock = Blocks.air.getDefaultState();
+		this.fillerBlock = Blocks.air.getDefaultState();
 	}
 }
