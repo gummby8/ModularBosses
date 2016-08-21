fd435bcbcad87be9a620970af4a0527652a5484f
Splosions <gummby8@gmail.com>
2016-08-21 00:51:50 -0700

    stuff

---
+++ main/java/com/Splosions/ModularBosses/entity/player/EntityRendererAlt.java
@@ -0,0 +1,48 @@
+package com.Splosions.ModularBosses.entity.player;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.resources.IResourceManager;
+
+public class EntityRendererAlt extends EntityRenderer
+{
+	private final Minecraft mc;
+	private float offsetY = 3.0F; // just for testing, should be based on actual render size
+
+	public EntityRendererAlt(Minecraft mc, IResourceManager resourceManager) {
+		super(mc, resourceManager); //, mc.getResourceManager());
+		this.mc = mc;
+	}
+
+	@Override
+	public void updateCameraAndRender(float partialTick) {
+		if (mc.thePlayer == null || mc.thePlayer.isPlayerSleeping()){
+			super.updateCameraAndRender(partialTick);
+			return;
+		}
+		// this is what changes the actual camera height
+		// but also seems to affect the player model render position
+		mc.thePlayer.renderOffsetY -= offsetY;
+		super.updateCameraAndRender(partialTick);
+		mc.thePlayer.renderOffsetY = 1.62F;
+	}
+
+	@Override
+	public void getMouseOver(float partialTick) {
+		if (mc.thePlayer == null || mc.thePlayer.isPlayerSleeping()){
+			super.getMouseOver(partialTick);
+			return;
+		}
+		// adjust the y position to get a mouseover at eye-level
+		// not perfect, as the server posY does not match, meaning
+		// that some block clicks do not process correctly
+		// (distance check or something like that)
+		mc.thePlayer.posY += offsetY;
+		mc.thePlayer.prevPosY += offsetY;
+		mc.thePlayer.lastTickPosY += offsetY;
+		super.getMouseOver(partialTick);
+		mc.thePlayer.posY -= offsetY;
+		mc.thePlayer.prevPosY -= offsetY;
+		mc.thePlayer.lastTickPosY -= offsetY;
+	}
+}
--- main/java/com/Splosions/ModularBosses/handler/MBEventHandler.java
+++ main/java/com/Splosions/ModularBosses/handler/MBEventHandler.java
@@ -2,9 +2,12 @@ package com.Splosions.ModularBosses.handler;
 
 import com.Splosions.ModularBosses.Config;
 import com.Splosions.ModularBosses.ModularBosses;
+import com.Splosions.ModularBosses.entity.player.EntityRendererAlt;
 import com.Splosions.ModularBosses.entity.player.MBExtendedPlayer;
 import com.Splosions.ModularBosses.proxy.ClientProxy;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.model.IBakedModel;
 import net.minecraft.client.resources.model.ModelResourceLocation;
@@ -17,41 +20,65 @@ import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class MBEventHandler {
 
+	private Minecraft mc;
+	private EntityRenderer renderer, prevRenderer;
+
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	public void onRenderTick(RenderTickEvent event) {
+		if (event.phase == TickEvent.Phase.START) {
+			if (renderer == null) {
+				renderer = new EntityRendererAlt(mc, mc.getResourceManager());
+			}
+			if (mc.entityRenderer != renderer) {
+				// be sure to store the previous renderer
+				prevRenderer = mc.entityRenderer;
+				mc.entityRenderer = renderer;
+			}
+		} else if (prevRenderer != null && mc.entityRenderer != prevRenderer) {
+			// reset the renderer
+			mc.entityRenderer = prevRenderer;
+		}
+
+	}
+
 	/*
-	 * used to make the player look ghostly when in limbo. 
+	 * used to make the player look ghostly when in limbo.
 	 */
 	@SideOnly(Side.CLIENT)
 	@SubscribeEvent
 	public void onRenderPlayer(RenderPlayerEvent.Pre event) {
-		if (MBExtendedPlayer.get((EntityPlayer)event.entity).preLimbo > 0){
+		if (MBExtendedPlayer.get((EntityPlayer) event.entity).preLimbo > 0) {
 			GlStateManager.enableBlend();
 			GlStateManager.disableAlpha();
 			GlStateManager.blendFunc(1, 1);
 		}
+
 	}
-	
+
 	@SideOnly(Side.CLIENT)
 	@SubscribeEvent
 	public void onRenderPlayer(RenderPlayerEvent.Post event) {
-        GlStateManager.disableBlend();
-        GlStateManager.enableAlpha();
+		GlStateManager.disableBlend();
+		GlStateManager.enableAlpha();
 	}
-	
-	
+
 	@SubscribeEvent
-	public void onEntityConstructing(EntityConstructing event){
+	public void onEntityConstructing(EntityConstructing event) {
 		if (event.entity instanceof EntityPlayer && MBExtendedPlayer.get((EntityPlayer) event.entity) == null)
 			MBExtendedPlayer.register((EntityPlayer) event.entity);
-		if (event.entity instanceof EntityPlayer && event.entity.getExtendedProperties(MBExtendedPlayer.EXT_PROP_NAME) == null)
-			event.entity.registerExtendedProperties(MBExtendedPlayer.EXT_PROP_NAME, new MBExtendedPlayer((EntityPlayer) event.entity));
+		if (event.entity instanceof EntityPlayer
+				&& event.entity.getExtendedProperties(MBExtendedPlayer.EXT_PROP_NAME) == null)
+			event.entity.registerExtendedProperties(MBExtendedPlayer.EXT_PROP_NAME,
+					new MBExtendedPlayer((EntityPlayer) event.entity));
 	}
-	
-	
+
 	@SubscribeEvent
 	public void onLivingUpdate(LivingUpdateEvent event) {
 		if (event.entity instanceof EntityPlayer) {
@@ -59,24 +86,23 @@ public class MBEventHandler {
 			MBExtendedPlayer.get(player).onUpdate();
 		}
 	}
-	
-	
 
-		
-	
 	@SideOnly(Side.CLIENT)
 	@SubscribeEvent
 	public void onBakeModel(ModelBakeEvent event) {
 		for (ModelResourceLocation resource : ClientProxy.smartModels.keySet()) {
-			Object object =  event.modelRegistry.getObject(resource);
+			Object object = event.modelRegistry.getObject(resource);
 			if (object instanceof IBakedModel) {
 				Class<? extends IBakedModel> clazz = ClientProxy.smartModels.get(resource);
 				try {
-					IBakedModel customRender = clazz.getConstructor(IBakedModel.class).newInstance((IBakedModel) object);
+					IBakedModel customRender = clazz.getConstructor(IBakedModel.class)
+							.newInstance((IBakedModel) object);
 					event.modelRegistry.putObject(resource, customRender);
-					ModularBosses.logger.warn("Registered new renderer for resource " + resource + ": " + customRender.getClass().getSimpleName());
+					ModularBosses.logger.warn("Registered new renderer for resource " + resource + ": "
+							+ customRender.getClass().getSimpleName());
 				} catch (NoSuchMethodException e) {
-					ModularBosses.logger.warn("Failed to swap model: class " + clazz.getSimpleName() + " is missing a constructor that takes an IBakedModel");
+					ModularBosses.logger.warn("Failed to swap model: class " + clazz.getSimpleName()
+							+ " is missing a constructor that takes an IBakedModel");
 				} catch (Exception e) {
 					ModularBosses.logger.warn("Failed to swap model with exception: " + e.getMessage());
 				}
@@ -85,5 +111,5 @@ public class MBEventHandler {
 			}
 		}
 	}
-	
+
 }
